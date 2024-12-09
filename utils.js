e5504eb28e7a18a102818a30cf79295325d7c035
한지호 <hjh05@sports2i.com>
2024-12-09 16:54:25 +0900

    Add: lodash 설치, random언어 적용

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 import path from "path";
 import fs from "fs";
 import axios from "axios";
+import lodash from "lodash";
 import { exec } from "child_process";
 import { openai } from "./index.js";
 import logger from "./logger.js";
@@ -29,17 +30,16 @@ export function formatNewsList(newsList) {
 
 // 3. OpenAI ChatGPT로 요청 보내기
 export async function generateCodeWithGpt(newsContent) {
+  const languages = ["C#", "Python", "Javascript", "Java"];
+  const randomLang = languages[lodash.random(0, languages.length - 1)];
   const ORDER = `
   You are a programming code generator that creates useful and straightforward code examples daily. Based on the latest news, choose one topic and write a sample code related to it, focusing on relevant libraries or algorithms.
   
   ${newsContent}
   
-  1. Randomly select one of the following programming languages: C#, Python, JavaScript, or Java.  
+  1. Randomly select one of the following programming languages: ${randomLang}.  
   2. The code must be practical, demonstrate a specific functionality, and use libraries or algorithms commonly applied in the industry.  
-  3. Write the following content in Korean:  
-    - **title**: A brief title explaining the purpose and functionality of the code.  
-    - **language**: The programming language used.  
-    - **content**: The entire code wrapped in proper Markdown code blocks with correct syntax highlighting for the chosen language.  
+  3. Write the following content in Korean
   4. Ensure all outputs are formatted precisely according to Markdown syntax.  
   5. Omit the initial \`\`\`markdown and ending \`\`\` outputs.  
   6. Use formal Korean writing, including expressions like "~이다."  
