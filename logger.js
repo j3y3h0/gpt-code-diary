abe292e974546edd1cfca62004f55cfdd2e1c289
한지호 <hjh05@sports2i.com>
2024-12-09 16:54:01 +0900

    Update: logger class화

--- logger.js
+++ logger.js
@@ -1,25 +1,67 @@
 import winston, { createLogger, transports } from "winston";
-import * as utils from "./utils.js";
 
 const { combine, timestamp, printf } = winston.format;
-const logDir = `${process.cwd()}/logs`;
 
-const generateFileName = () => {
-  const today = utils.getyyyyMMdd();
-  return `${today}.log`;
-};
+class Logger {
+  constructor() {
+    this.logDir = `${process.cwd()}/logs`;
+    this.logger = this.createLoggerInstance();
+  }
 
-/** 날짜 로그레벨 메세지 */
-const logFormat = printf(({ level, message, timestamp }) => {
-  return `${timestamp} ${level}: ${message}`;
-});
+  // 로그 파일 이름 생성
+  generateFileName() {
+    const DATE_OPTION = { timeZone: "Asia/Seoul" };
+    const nowDate = new Date().toLocaleString("en-US", DATE_OPTION);
+    const year = new Date(nowDate).getFullYear();
+    const month = String(new Date(nowDate).getMonth() + 1).padStart(2, "0");
+    const day = String(new Date(nowDate).getDate()).padStart(2, "0");
+    return `${year}${month}${day}.log`;
+  }
 
-const logger = createLogger({
-  level: "info",
-  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), logFormat),
-  transports: [
-    new transports.File({ filename: generateFileName(), dirname: logDir }),
-  ],
-});
+  // 로그 형식 정의
+  getLogFormat() {
+    return printf(({ level, message, timestamp }) => {
+      return `${timestamp} ${level}: ${message}`;
+    });
+  }
 
-export default logger;
+  // Logger 인스턴스 생성
+  createLoggerInstance() {
+    return createLogger({
+      level: "info", // 로그 레벨 설정
+      format: combine(
+        timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), // 타임스탬프 추가
+        this.getLogFormat() // 커스텀 포맷 사용
+      ),
+      transports: [
+        new transports.File({
+          filename: this.generateFileName(), // 파일 이름 생성
+          dirname: this.logDir, // 로그 디렉터리 설정
+        }),
+      ],
+    });
+  }
+
+  // 로그 기록 함수
+  log(level, message) {
+    this.logger.log({ level, message });
+  }
+
+  // info 로그 기록 함수
+  info(message) {
+    this.log("info", message);
+  }
+
+  // error 로그 기록 함수
+  error(message) {
+    this.log("error", message);
+  }
+
+  // warn 로그 기록 함수
+  warn(message) {
+    this.log("warn", message);
+  }
+}
+
+// Logger 클래스 인스턴스를 생성해서 내보냄
+export default new Logger();
