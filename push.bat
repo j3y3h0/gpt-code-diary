fe194372a29da66aba737cf9969c42051e4bc098
j3y3h0 <mlpzaq123456@naver.com>
2025-06-29 12:27:11 +0900

    fix: push.bat 시스템 변경으로 인한 배치 스크립트 코드 변경, 시간 구하는 명령어 호환 안되었음

--- push.bat
+++ push.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 :: 현재 디렉토리에서 변경 사항을 커밋하고 푸시하는 스크립트
 
 :: Git 상태 확인
@@ -7,22 +8,18 @@
 :: 변경된 파일을 스테이징
 git add .
 
-:: 현재 날짜 및 시간 구하기 (yyyyMMddHHmm 형식)
-for /f "tokens=2 delims==" %%I in ('"wmic OS Get localdatetime /value"') do set datetime=%%I
-set year=%datetime:~0,4%
-set month=%datetime:~4,2%
-set day=%datetime:~6,2%
-set hour=%datetime:~8,2%
-set minute=%datetime:~10,2%
+:: PowerShell로 날짜 및 시간 구하기 (yyyyMMddHHmm)
+for /f %%i in ('powershell -Command "Get-Date -Format yyyyMMddHHmm"') do set commitMessage=%%i
 
-:: 커밋 메시지 생성 (yyyyMMddHHmm 형식)
-set commitMessage=%year%%month%%day%%hour%%minute%
+:: 커밋 메시지 확인
+:: echo commitMessage: !commitMessage!
 
 :: 커밋
-git commit -m "Save: %commitMessage%"
+git commit -m "Save: !commitMessage!"
 
 :: 푸시
 git push
 
 :: 작업 완료 메시지
 echo Commit and push complete.
+:: pause
